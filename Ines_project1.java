87ae5cb849d839552b6f44bd54806689fce73e2b
InesAnindiyta <anindyta965@gmail.com>
2025-08-30 21:14:54 +0700

    Add files via upload

---
+++ Ines_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ines_project1;
+
+/**
+ *
+ * @author User
+ */
+public class Ines_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Ines Anindiyta");
+        System.out.println("NPM : 2440506065");
+        System.out.println("Alamat : salam,magelang");
+        System.out.println("No_hp : 081111111111");
+    }
+}
