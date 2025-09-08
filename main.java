33e3ddaaaca01ba190226a2739c5054b32df6067
InesAnindiyta <anindyta965@gmail.com>
2025-09-08 22:10:39 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum3;
+
+/**
+ *
+ * @author User
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hewan kucing = new Hewan();
+        kucing.nama = "mimi";
+        kucing.umur = 3;
+        kucing.suara();
+    }
+    
+}
