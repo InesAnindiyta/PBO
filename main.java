d437a5112ad19186b536ce94603a17680c899115
InesAnindiyta <anindyta965@gmail.com>
2025-09-08 22:12:11 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package praktikum3;
+package tugas3;
 
 /**
  *
@@ -14,10 +14,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Hewan kucing = new Hewan();
-        kucing.nama = "mimi";
-        kucing.umur = 3;
-        kucing.suara();
+        // TODO code application logic here
     }
     
 }
