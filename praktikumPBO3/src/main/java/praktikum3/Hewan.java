540501cc8339d7c4beece96478db93d0fdb24867
InesAnindiyta <anindyta965@gmail.com>
2025-09-22 20:44:47 +0700

    Add files via upload

---
+++ praktikumPBO3/src/main/java/praktikum3/Hewan.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum3;
+
+/**
+ *
+ * @author User
+ */
+public class Hewan {
+    String nama;
+    int umur;
+    
+    void suara() {
+        System.out.println("hewan bersuara");
+    }
+}
