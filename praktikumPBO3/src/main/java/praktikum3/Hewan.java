fdf6340db21f24e2b0a74ab5f0996c86ad78ac21
InesAnindiyta <anindyta965@gmail.com>
2025-09-24 17:32:27 +0700

    Hewan.java

--- praktikumPBO3/src/main/java/praktikum3/Hewan.java
+++ praktikumPBO3/src/main/java/praktikum3/Hewan.java
@@ -9,10 +9,34 @@ package praktikum3;
  * @author User
  */
 public class Hewan {
-    String nama;
-    int umur;
+    private String nama;
+    private int umur;
     
-    void suara() {
+    public hewan(String nama, int umur) {
+        this.nama = nama;
+        this.umur = umur;
+    }
+    public String getName() {
+        return nama;
+    }
+    public void setName(String nama) {
+        this.nama = nama;
+    }
+    public int getUmur() {
+        return umur;
+    }
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    public void suara() {
         System.out.println("hewan bersuara");
     }
+    public void info() {
+        System.out.println("Nama hewan: " + nama);
+        System.out.println("Umur hewan: " + umur);
+    }
+    public void berlari () {
+        System.out.println("hewan sedang berlari");
+    }
 }
+
