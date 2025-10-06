e20bf0bc72451ba27f870b10673a9491f0c79f71
InesAnindiyta <anindyta965@gmail.com>
2025-10-06 08:21:51 +0700

    Add files via upload

---
+++ responsiUTS-InesAnindiyta/src/main/java/com/mycompany/responsiuts/inesanindiyta/ResponsiUTSInesAnindiyta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.responsiuts.inesanindiyta;
+
+/**
+ *
+ * @author User
+ */
+public class ResponsiUTSInesAnindiyta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
